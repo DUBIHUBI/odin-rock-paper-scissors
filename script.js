4ab55a736ae8d12bc8961d2af6c5f42c2be9666e
Hubert Yu <hubert12161999@gmail.com>
2022-12-06 22:25:51 +0000

    Add 3 working buttons and display log of results

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
     case 0:
-      choice = "rock";
+      choice = 'rock';
       break;
     case 1:
-      choice = "paper";
+      choice = 'paper';
       break;
     case 2:
-      choice = "scissors";
+      choice = 'scissors';
       break;
     default:
   }
@@ -21,39 +21,57 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   let winnerMessage;
-  if (playerSelection === "rock" && computerSelection === "scissors" ||
-     playerSelection === "paper" && computerSelection === "rock" ||
-     playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++
-    winnerMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else if (computerSelection === "rock" && playerSelection === "scissors" ||
-            computerSelection === "paper" && playerSelection === "rock" ||
-            computerSelection === "scissors" && playerSelection === "paper") {
-    computerScore++
-    winnerMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+  if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    playerScore++;
+    winnerMessage = `You Win! ${playerSelection} beats ${computerSelection}.`;
+  } else if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper')
+  ) {
+    computerScore++;
+    winnerMessage = `You Lose! ${computerSelection} beats ${playerSelection}.`;
   } else {
-    winnerMessage = `You Draw!`;
+    winnerMessage = `It's a tie! You both chose ${playerSelection}.`;
   }
   return winnerMessage;
 }
 
-function game() {
-  playerScore = 0;
-  computerScore = 0;
-  let gameMessage
-  for (let i = 0; i < 5; i++) {
-    // console.log(i);
-    console.log(`Round ${i + 1} out of 5!`)
-    playerSelection = prompt("Please choose from: rock, paper, scissors");
-    console.log(playRound(playerSelection, getComputerChoice()));
-  }
+// function game() {
+//   playerScore = 0;
+//   computerScore = 0;
+//   let gameMessage;
+//   // for (let i = 0; i < 5; i++) {
+//   // console.log(i);
+//   // console.log(`Round ${i + 1} out of 5!`)
+//   playerSelection = prompt('Please choose from: rock, paper, scissors');
+//   console.log(playRound(playerSelection, getComputerChoice()));
+//   // }
 
-  if (playerScore > computerScore) {
-    gameMessage = `Congratulations! You Won ${playerScore} - ${computerScore}.`
-  } else if (computerScore > playerScore) {
-    gameMessage = `You Lost ${playerScore} - ${computerScore}.`
-  } else {
-    gameMessage = `You Drew ${playerScore} - ${computerScore}.`
-  }
-  console.log(gameMessage)
-}
+//   if (playerScore > computerScore) {
+//     gameMessage = `Congratulations! You Won ${playerScore} - ${computerScore}.`;
+//   } else if (computerScore > playerScore) {
+//     gameMessage = `You Lost ${playerScore} - ${computerScore}.`;
+//   } else {
+//     gameMessage = `You Drew ${playerScore} - ${computerScore}.`;
+//   }
+//   console.log(gameMessage);
+// }
+
+const buttons = document.querySelectorAll('button');
+const gameLog = document.querySelector('.game-log');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const result = document.createElement('div');
+    result.classList.add('result');
+    result.textContent = playRound(e.target.className, getComputerChoice());
+    gameLog.appendChild(result);
+
+    // console.log(gameLog);
+  });
+});
